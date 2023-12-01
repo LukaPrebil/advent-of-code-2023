d7124803f5828367afa135a6208e74988b5ea442
Luka Prebil Grintal <pgluka7@gmail.com>
2023-12-01 07:57:03 +0100

    Structure

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { d1p1, d1p2 } from "./day1/part1.js";
+import { d1p1 } from "./day1/part1.js";
+import { d1p2 } from "./day1/part2.js";
 
 console.log("Day 1 part 1:", await d1p1());
 console.log("Day 1 part 2:", await d1p2());
